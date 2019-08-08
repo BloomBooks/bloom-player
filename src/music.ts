32d20676552fb00f78e4c7c9cb2af7110a582084
Stephen McConnel <stephen_mcconnel@sil.org>
2019-08-08 13:49:48 -0600

    Fix background music to continue as desired on adjacent pages (BL-7493)
    Also fix volume setting to be more accurate for background music.

--- src/music.ts
+++ src/music.ts
@@ -6,6 +6,8 @@ export class Music {
     public urlPrefix: string;
     private paused: boolean = false;
     private currentPage: HTMLDivElement;
+    private playingBackgroundAudio: string; // data-backgroundaudio currently playing
+    private playingBackgroundAudioPage: string; // corresponding data-page-number or data-xmatter-page
 
     public static documentHasMusic(): boolean {
         return [].slice
@@ -19,21 +21,26 @@ export class Music {
 
     public HandlePageVisible(bloomPage: HTMLElement) {
         this.currentPage = bloomPage as HTMLDivElement;
-        if (!this.currentPage || !Music.pageHasMusic(this.currentPage)) {
-            return;
-        }
-        this.setMusicSource();
-        if (!this.paused) {
-            this.play();
+        if (this.currentPage && Music.pageHasMusic(this.currentPage)) {
+            this.listen();
         }
     }
 
     public hidingPage() {
-        this.pause();
+        // music continues on the next page by default.
+    }
+
+    private listen() {
+        this.setMusicSourceAndVolume();
+        if (this.paused) {
+            this.getPlayer().pause();
+        } else {
+            this.getPlayer().play();
+        }
     }
 
     public play() {
-        if (!this.currentPage || !Music.pageHasMusic(this.currentPage)) {
+        if (!this.currentPage) {
             return;
         }
         this.getPlayer().play();
@@ -41,7 +48,7 @@ export class Music {
     }
 
     public pause() {
-        if (!this.currentPage || !Music.pageHasMusic(this.currentPage)) {
+        if (!this.currentPage) {
             return;
         }
         this.getPlayer().pause();
@@ -78,14 +85,69 @@ export class Music {
     // so it will cache the previous content of the file or
     // remember if no such file previously existed. So we add a bogus query string
     // based on the current time so that it asks the server for the file again.
-    private setMusicSource(): void {
-        const player = this.getPlayer();
+    private setMusicSourceAndVolume(): void {
+        const music = this.currentPage.attributes["data-backgroundaudio"].value;
+        const page = this.getPageNumberOrLabel();
+        if (
+            music === this.playingBackgroundAudio &&
+            page === this.playingBackgroundAudioPage
+        ) {
+            // We've returned to the page that established the currently playing
+            // background audio.  We don't want to reset the audio src because that
+            // starts over from the beginning.  Once started we want a continuous
+            // stream until another source is chosen in the book.  On the other hand,
+            // if the user explicitly uses the same audio file again then presumably
+            // the desire is to start from the beginning again.
+            return;
+        }
         const url = this.currentMusicUrl(
-            this.currentPage.attributes["data-backgroundaudio"].value +
-                "?nocache=" +
-                new Date().getTime()
+            music + "?nocache=" + new Date().getTime()
         );
-        player.setAttribute("src", url);
+        // console.log(
+        //     "DEBUG: Music.setMusicSource() src = '" +
+        //         (music ? url : music) +
+        //         "'"
+        // );
+        const player = this.getPlayer();
+        player.setAttribute("src", music ? url : "");
+        this.playingBackgroundAudio = music;
+        this.playingBackgroundAudioPage = page;
+        const volume = this.getMusicVolume();
+        if (volume.length) {
+            player.volume = Number(volume);
+        }
+    }
+
+    private getMusicVolume(): string {
+        if (!this.currentPage) {
+            return "";
+        }
+        const volume = this.currentPage.getAttribute(
+            "data-backgroundaudiovolume"
+        );
+        if (!volume) {
+            return "";
+        }
+        return volume;
+    }
+
+    private getPageNumberOrLabel(): string {
+        if (!this.currentPage) {
+            return "";
+        }
+        const dataPageNumber = this.currentPage.getAttribute(
+            "data-page-number"
+        );
+        if (dataPageNumber) {
+            return dataPageNumber;
+        }
+        const dataXmatterPage = this.currentPage.getAttribute(
+            "data-xmatter-page"
+        );
+        if (dataXmatterPage) {
+            return dataXmatterPage;
+        }
+        return "";
     }
 
     private currentMusicUrl(filename: string): string {
@@ -94,6 +156,7 @@ export class Music {
 
     private playEnded() {
         // just start playing all over again.
+        this.getPlayer().currentTime = 0;
         this.play();
     }
 }
