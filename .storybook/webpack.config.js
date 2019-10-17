277bf402c5e1ab8348a3b68a9228140a82888ee2
John Hatton <hattonjohn@gmail.com>
2019-10-17 13:59:23 -0600

    Hide storybook webpack progress noise

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,5 +5,9 @@ const core = require("../webpack.core.js");
 module.exports = ({ config }) => {
     config.module.rules.push(...core.module.rules);
     config.resolve.extensions.push(".ts", ".tsx");
+    // hide the noisy webpack.progress lines. See https://github.com/storybookjs/storybook/issues/1872
+    config.plugins = config.plugins.filter(
+        ({ constructor }) => constructor.name !== "ProgressPlugin"
+    );
     return config;
 };
