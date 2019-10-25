3ae73f5692a5a8cbac9f4e01ff6961d19d41af89
Gordon Martin <martigor@gmail.com>
2019-10-25 11:09:33 -0700

    BL-7610 Switch to building lg menu from DOM
    * Look for text in the DOM using logic from the C#
       Book.AllLanguages property
    * Still use the meta.json file to inform preferred name
       and menu order (put L1 at the top)

--- src/langData.ts
+++ src/langData.ts
@@ -79,40 +79,124 @@ export default class LangData {
         body: HTMLBodyElement,
         metadataObject: object
     ): LangData[] {
-        const result: LangData[] = [];
-        if (!metadataObject.hasOwnProperty("language-display-names")) {
-            const fallbackLangData = LangData.getMinimalFallbackL1(body);
-            result.push(fallbackLangData);
-            return result;
-        }
+        let languageArray: LangData[] = [];
         const languageDisplayNames: object =
             metadataObject["language-display-names"];
-        for (const code in languageDisplayNames) {
-            if (languageDisplayNames.hasOwnProperty(code)) {
-                let displayName: string = languageDisplayNames[code];
-                displayName = LangData.getBestLanguageName(code, displayName);
-                const langData = new LangData(displayName, code);
-                if (LangData.hasAudioInLanguage(body, code)) {
+        // We are largely imitating the logic in the C# property Book.AllLanguages, so if changes
+        // are needed here, we may need changes there too.
+        // This xpath gets us editable divs that are in non-xMatter pages and have
+        // a potentially interesting language.
+        let xpath = "//div[contains(@class, 'bloom-page') and";
+        xpath +=
+            " not(contains(@class, 'bloom-frontMatter')) and not(contains(@class, 'bloom-backMatter'))]";
+        xpath += "//div[contains(@class, 'bloom-editable') and @lang]";
+        const langDivs = body.ownerDocument!.evaluate(
+            xpath,
+            body,
+            null,
+            XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+            null
+        );
+        for (let idx = 0; idx < langDivs.snapshotLength; idx++) {
+            const langDiv = langDivs.snapshotItem(idx) as HTMLElement;
+            if (
+                langDiv.parentElement!.classList.contains(
+                    "bloom-ignoreChildrenForBookLanguageList"
+                )
+            ) {
+                continue;
+            }
+            const langCode = langDiv.getAttribute("lang");
+            // Not valid languages, though we sometimes use them for special purposes
+            if (
+                !langCode ||
+                langCode === "z" ||
+                langCode === "*" ||
+                langCode === ""
+            ) {
+                continue;
+            }
+            if (LangData.getLangDataByCode(languageArray, langCode)) {
+                // We already have this one!
+                continue;
+            }
+            const divText = langDiv.innerText.trim();
+            if (divText) {
+                // We have text in this language and we don't already have this one "on file".
+                // Try to get a display name from the meta.json object.
+                const langData = new LangData(
+                    LangData.getBestLanguageNameFromMetaData(
+                        langCode,
+                        languageDisplayNames
+                    ),
+                    langCode
+                );
+                if (LangData.hasAudioInLanguage(body, langCode)) {
                     langData.HasAudio = true;
                 }
-                result.push(langData);
+                languageArray.push(langData);
             }
         }
-        // Apply fallback information if we need it
-        if (result.length === 0 || result[0].Name === result[0].Code) {
+        // Reorder based on Collection settings, then alphabetically.
+        languageArray = LangData.reorderMenuItems(
+            languageDisplayNames,
+            languageArray
+        );
+
+        // Apply fallback information if we need it, but it's now very unlikely we'll need this
+        // because this means that NO language code was found to have ANY text outside of xMatter.
+        if (languageArray.length === 0) {
             const fallbackLangData = LangData.getMinimalFallbackL1(body);
-            if (result.length === 0) {
-                result.push(fallbackLangData);
-            } else {
-                result[0].name = fallbackLangData.Name;
-            }
+            languageArray.push(fallbackLangData);
         }
         // Always assume the first is selected to begin with
-        result[0].IsSelected = true;
-        return result;
+        languageArray[0].IsSelected = true;
+        return languageArray;
+    }
+
+    // Reorder based on Collection settings. Put L1 at the top of the menu,
+    // the rest alphabetically according to display name in current locale.
+    private static reorderMenuItems(
+        languageDisplayNames: object,
+        languageArray: LangData[]
+    ) {
+        languageArray = languageArray.sort(LangData.compare);
+        const keys = Object.keys(languageDisplayNames);
+        if (keys.length === 0) {
+            return languageArray;
+        }
+        const firstLanguage = keys[0];
+        if (LangData.getLangDataByCode(languageArray, firstLanguage)) {
+            LangData.moveToBeginning(languageArray, firstLanguage);
+        }
+        return languageArray;
+    }
+
+    private static compare(a: LangData, b: LangData): number {
+        const aName = a.name.toLocaleLowerCase();
+        const bName = b.name.toLocaleLowerCase();
+        if (aName === bName) {
+            return 0;
+        }
+        if (aName > bName) {
+            return 1;
+        }
+        return -1;
+    }
+
+    // Move the LangData referenced by code to the beginning of the array
+    private static moveToBeginning(languageArray: LangData[], code: string) {
+        const langDataToReorder = LangData.getLangDataByCode(
+            languageArray,
+            code
+        );
+        const index = languageArray.indexOf(langDataToReorder);
+        // move from old index to beginning of array
+        languageArray.splice(0, 0, languageArray.splice(index, 1)[0]);
     }
 
     private static getMinimalFallbackL1(body: HTMLBodyElement): LangData {
+        // This is now only used in the unlikely case that no language has text in the book!
         const dataDivElement = body.ownerDocument!.getElementById(
             "bloomDataDiv"
         );
@@ -146,10 +230,18 @@ export default class LangData {
         name = LangData.getBestLanguageName(code, name);
         const fallback = new LangData(name, code);
         fallback.IsSelected = true;
-        if (LangData.hasAudioInLanguage(body, code)) {
-            fallback.HasAudio = true;
-        }
         return fallback;
+    }
+
+    private static getBestLanguageNameFromMetaData(
+        code: string,
+        languageDisplayNames: object
+    ): string {
+        let proposedDisplayName = "";
+        if (languageDisplayNames.hasOwnProperty(code)) {
+            proposedDisplayName = languageDisplayNames[code];
+        }
+        return LangData.getBestLanguageName(code, proposedDisplayName);
     }
 
     private static getBestLanguageName(
@@ -196,30 +288,27 @@ export default class LangData {
         isoCode: string
     ): boolean {
         let result = false;
-        const audioDivs =
-            body.ownerDocument!.evaluate(
-                ".//div[@lang='" +
-                    isoCode +
-                    "']//span[contains(@class, 'audio-sentence')]",
-                body,
-                null,
-                XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-                null
-            );
-            for (let idx = 0; idx < audioDivs.snapshotLength; idx++) {
-                const audioSpan = audioDivs.snapshotItem(
-                    idx
-                ) as HTMLElement;
-                const divParent = audioSpan.closest("div");
-                if (divParent === null) {
-                    continue;
-                }
-                if (divParent.getAttribute("data-book") !== null) {
-                    continue;
-                }
-                result = true;
-                break;
+        const audioDivs = body.ownerDocument!.evaluate(
+            ".//div[@lang='" +
+                isoCode +
+                "']//span[contains(@class, 'audio-sentence')]",
+            body,
+            null,
+            XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+            null
+        );
+        for (let idx = 0; idx < audioDivs.snapshotLength; idx++) {
+            const audioSpan = audioDivs.snapshotItem(idx) as HTMLElement;
+            const divParent = audioSpan.closest("div");
+            if (divParent === null) {
+                continue;
             }
+            if (divParent.getAttribute("data-book") !== null) {
+                continue;
+            }
+            result = true;
+            break;
+        }
         return result;
     }
 }
