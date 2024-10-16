8bc50faa8764538dc34c9f43783d555ec2eee2f2
Hatton <hattonjohn@gmail.com>
2024-10-16 15:22:08 -0600

    chore: fresh storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended", "plugin:storybook/recommended"],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
