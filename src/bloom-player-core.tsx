c4c0928cd5160ef97005b410aa8d9ae9c5d51b3c
John Thomson <john_thomson@sil.org>
2025-08-19 12:32:19 -0500

    fix: use original page size if book has any overlays (BL-15140)

--- src/bloom-player-core.tsx
+++ src/bloom-player-core.tsx
@@ -254,6 +254,7 @@ export class BloomPlayerCore extends React.Component<IProps, IPlayerState> {
 
     private bookInfo: BookInfo = new BookInfo();
     private bookInteraction: BookInteraction = new BookInteraction();
+    private mustUseOriginalPageSize: boolean = false;
 
     private static currentPagePlayer: BloomPlayerCore;
     private indicesOfPagesWhereWeShouldPreserveDOMState: any = {};
@@ -707,6 +708,8 @@ export class BloomPlayerCore extends React.Component<IProps, IPlayerState> {
 
                         const body =
                             bookHtmlElement.getElementsByTagName("body")[0];
+                        this.mustUseOriginalPageSize =
+                            this.computeMustUseOriginalPageSize(body);
 
                         this.bookInfo.setSomeBookInfoFromBody(body);
                         // contains conditions to limit this to one time only after assembleStyleSheets has completed.
@@ -1357,6 +1360,20 @@ export class BloomPlayerCore extends React.Component<IProps, IPlayerState> {
         ) as HTMLElement[];
     }
 
+    // We must use the original page size if the book has canvas elements. We used to detect this
+    // by looking for the 'comical' feature in meta.json, but at some point we limited this
+    // feature to books that have actual comicaljs overlays and which the author claims to
+    // be comics during publishing. But any kind of overlay will be wrongly positioned if
+    // we change the page size under it. So now we have to actually look for such elements.
+    // And unfortunately we may encounter old books that use the old class for canvas elements.
+    private computeMustUseOriginalPageSize(body: HTMLElement): boolean {
+        if (body.querySelector(".bloom-canvas-element")) {
+            return true;
+        }
+        // for older books, we're looking for this class
+        return !!body.querySelector(".bloom-textOverPicture");
+    }
+
     private updateOverlayPositionsByLangCode(): void {
         if (!this.props.activeLanguageCode || !this.htmlElement) {
             return; // shouldn't happen, just a precaution
@@ -1626,7 +1643,7 @@ export class BloomPlayerCore extends React.Component<IProps, IPlayerState> {
             page,
             this.bookInfo.canRotate,
             this.props.landscape,
-            this.props.useOriginalPageSize || this.bookInfo.hasFeature("comic"),
+            this.props.useOriginalPageSize || this.mustUseOriginalPageSize,
             this.originalPageClass,
         );
     }
